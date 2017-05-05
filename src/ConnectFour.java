a612a0ee2a98872f92212046730add2715ea6520
18owenst <18owenst@da.org>
2017-05-05 14:14:53 -0400

    Initial Commit
    Original files for ConnectFour project.

---
+++ src/ConnectFour.java
@@ -0,0 +1,52 @@
+
+public class ConnectFour implements BoardGame {
+
+	public ConnectFour() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void newGame() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean gameOver() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int getWinner() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Position[] getWinningPositions() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean columnFull(int column) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void play(int column) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int[][] getBoard() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+
+}
